eb67c7fc7ee26c6aa96b0740770a8835245b4fb1
NastiaMikhaltsova <mihaltsovan@gmail.com>
2017-12-26 09:31:32 +0200

    test

---
+++ IceWorldTests/KievFactoryTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IceWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceWorld.Tests
+{
+    [TestClass()]
+    public class KievFactoryTests
+    {
+        [TestMethod()]
+        public void CreateIceTest()
+        {
+            KievFactory leo = new KievFactory();
+            IIce max = leo.CreateIce();
+            Assert.IsTrue(max is KievIce);
+            Assert.IsTrue(max.Weight > 50 && max.Weight <= 500);
+        }
+
+        [TestMethod()]
+        public void CreateCakeTest()
+        {
+            KievFactory leo = new KievFactory();
+            ICake max = leo.CreateCake();
+            Assert.IsTrue(leo is KievFactory);
+            Assert.IsTrue(max.Weight > 800);
+            Assert.IsTrue(max is KievCake);
+            Assert.IsTrue(max.Diameter == 32);
+            Assert.AreNotSame(leo, max);
+        }
+    }
+}
---
+++ IceWorldTests/OdessaFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IceWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceWorld.Tests
+{
+    [TestClass()]
+    public class OdessaFactoryTests
+    {
+        [TestMethod()]
+        public void CreateCakeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateIceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOrderTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
